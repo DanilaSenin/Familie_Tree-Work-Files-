1a2fbccd6b719f137450ee9cb67f5dc1b1a1252f
Daniil_Senin <16502711d.ru@gmail.com>
2024-04-11 14:41:42 +0300

    Alfa Version

--- src/Family_Tree/Human.java
+++ src/Family_Tree/Human.java
@@ -1,25 +1,30 @@
 package Family_Tree;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Human {
     private String name, gender;
     private LocalDate birthDate, deathDate;
-    private List<Human> children;
+    private List<Human> children = new ArrayList<>();
     private Human mather, father;
-    private Integer Age;
+    private Period age;
 
+    public Human(){
+        this.name = "No data available";
+    }
     public Human(String name, LocalDate birthDate, String gender) {
         this.name = name;
         this.birthDate = birthDate;
         this.gender = gender;
-        this.mather = mather;
-        this.father = father;
+        this.age = birthDate.until(LocalDate.now());
         }
     public Human(String name, LocalDate birthDate, LocalDate deathDate, String gender) {
         this(name, birthDate, gender);
         this.deathDate = deathDate;
+        this.age = birthDate.until(deathDate);
     }
 
     public LocalDate getBirthDate() {
@@ -42,17 +47,28 @@ public class Human {
         return gender;
     }
 
-    public void SjetAge() {
-        if (this.deathDate != null) {
-            this.Age = LocalDate.now().getYear() - this.birthDate.getYear();
-        }
-        else {
-            this.Age = this.deathDate.getYear() - this.birthDate.getYear();
-        }
+    public void setFather(Human father) {
+        this.father = father;
+    }
+    public Human getFather() {
+        return father;
+    }
+    public void setMather(Human mather) {
+        this.mather = mather;
+    }
+    public Human getMather() {
+        return mather;
+    }
+
+    public void addChild(Human child) {
+        children.add(child);
+    }
+    public List<Human> getChildren() {
+        return children;
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + "\n" + "Gender: " + gender + " \n Date of Birth: " + birthDate  + "\n" + " Date of Death: " + deathDate + "\n" +"\n" + "Age: " + Age;
+        return "\n Name: " + name + "\n" + "Gender: " + gender + " \n Date of Birth: " + birthDate  + "\n" + " Date of Death: " + deathDate + "\n"  + "Age: " + age.getYears() +"\n" + "Mother: " + mather +"\n" + "Father: " + father +"\n";
     }
 }
--- src/Family_Tree/Main.java
+++ src/Family_Tree/Main.java
@@ -4,14 +4,37 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        FamilyTree vendingMachine = new FamilyTree("Arkan Second Tverd");
+        //Название семьи (Фамилия)
+        FamilyTree Family = new FamilyTree("Svers");
 
-        Human human1 = new Human("Arkan Second Tverd", LocalDate.now(), "Man");
-        Human human2 = new Human("Miram Second Tverd", LocalDate.of(1904, 12, 11), LocalDate.of(2022, 12,13), "Woman");
+        //Родители семьи
+        Human human1 = new Human("Jon", LocalDate.of(1905, 11, 15), "Man");
+        Human human2 = new Human("Jane", LocalDate.of(1904, 12, 11), LocalDate.of(2022, 12,13), "Woman");
 
-        vendingMachine.addPerson(human1);
-        vendingMachine.addPerson(human2);
+        // Дети
+        //Заносим детей в книгу
+        Human human3 = new Human("Uil", LocalDate.of(1953, 8, 17), "Man");
+        Human human4 = new Human("Janin", LocalDate.of(1955, 5, 21), "Woman");
+        //Отмечаем родителей каждого ребёнка
 
-        System.out.println(vendingMachine.getPersonInfo());
+        human3.setFather(human1);
+        human3.setMather(human2);
+
+        human4.setFather(human1);
+        human4.setMather(human2);
+
+        //Заносим к каждому родителю их детей
+        human1.addChild(human3);
+        human1.addChild(human4);
+
+        human2.addChild(human3);
+        human2.addChild(human4);
+
+
+        Family.addPerson(human1);
+        Family.addPerson(human2);
+        Family.addPerson(human3);
+        Family.addPerson(human4);
+        System.out.println(Family.getPersonInfo());
     }
 }
